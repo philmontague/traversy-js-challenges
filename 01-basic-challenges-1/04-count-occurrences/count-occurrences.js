96fc5913d80cbb266d02d3579f681318ac6a3ac4
Phil <pamramfam@gmail.com>
2023-10-13 16:18:56 -0400

    updated up to find-max-number

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,16 @@
-function countOccurrences() {}
+// function countOccurrences(str, char) {
+//     let count = 0; 
+
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === char) { 
+//             count++; 
+//         }
+//     }
+//     return count; 
+// }
+
+const countOccurrences = (str, char) => str.split(char).length - 1; 
+
+
 
 module.exports = countOccurrences;
